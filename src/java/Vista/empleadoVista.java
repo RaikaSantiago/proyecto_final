c886223aec639014375780772574ddcd1281fdc7
jhon cortes <cortes.jhon@correounivalle.edu.co>
2019-07-22 13:05:22 -0500

    paquete persistencia

---
+++ src/java/Vista/empleadoVista.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author usuario
+ */
+@Named(value = "empleadoVista")
+@RequestScoped
+public class empleadoVista {
+
+    /**
+     * Creates a new instance of empleadoVista
+     */
+    public empleadoVista() {
+    }
+    
+}

d4b95a4ae1ab0e480c1e9564652aa558c897d72f
RaikaSantiago <https://github.com/RaikaSantiago/Taller1JavaWeb.git>
2019-08-10 10:09:59 -0500

    Santiago - Pack Logica

---
+++ src/java/Logica/AsignacionesLogica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author ALEJANDRA
+ */
+@Stateless
+public class AsignacionesLogica implements AsignacionesLogicaLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}

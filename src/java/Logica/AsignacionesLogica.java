8dbd482f2acd664528e9585a0c69c14830ec6a73
RaikaSantiago <https://github.com/RaikaSantiago/Taller1JavaWeb.git>
2019-08-10 15:57:50 -0500

    Santiago - Asignaciones CRUD

--- src/java/Logica/AsignacionesLogica.java
+++ src/java/Logica/AsignacionesLogica.java
@@ -6,16 +6,33 @@
 package Logica;
 
 import Modelo.Asignaciones;
+import Modelo.Empleados;
+import Persistencia.AsignacionesFacadeLocal;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 
 @Stateless
 public class AsignacionesLogica implements AsignacionesLogicaLocal {
-
+     @EJB
+    public AsignacionesFacadeLocal asignacionesDAO;
+    
     @Override
     public void registrarAsignaciones(Asignaciones c) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (c == null) {
+            throw new Exception("La asignacion ha realizar no tiene información");
+        }
+        if (c.getHoras().equals("")){
+          throw new Exception ("El campo horas no puede ir vacio");
+        }
+        if (c.getCantidadProyectos().equals("")){
+          throw new Exception ("Agregue la cantidad de proyectos");
+        }
+        if (c.getPresupuestos().equals("")){
+          throw new Exception ("El presupuesto debe estar digitado");
+        }
+        asignacionesDAO.create((Asignaciones) c);
     }
 
     @Override
@@ -25,17 +42,39 @@ public class AsignacionesLogica implements AsignacionesLogicaLocal {
 
     @Override
     public List<Asignaciones> consultaAsignaciones() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return asignacionesDAO.findAll(); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void modificarAsignaciones(Asignaciones c) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        if(c==null){
+            throw new Exception("El Nit hacido modificado");
+        }
+        if(c.getCantidadProyectos()==0){
+            throw new Exception("La cantidad de proyectos  ha sido modificado");
+        }
+        if(c.getHoras().equals("")){
+            throw new Exception("Las horas fueron modificadas");
+        }
+        Asignaciones objContratista = asignacionesDAO.find(c.getAsignacionesPK());
+        if(objContratista == null){
+            throw new Exception("La asignacion  ha sido Borrada");
+        }
+        asignacionesDAO.edit(c);
     }
 
     @Override
     public void eliminarAsignaciones(Asignaciones c) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   
+       Asignaciones objBorrar = asignacionesDAO.find(c.getAsignacionesPK());
+       if(objBorrar==null){
+         throw new Exception("La asignacion no existe");  
+       }
+//       if(objBorrar.getEmpleadosList().size()>0 || objBorrar.getIngresoList().size()>0 ){
+//         throw new Exception("El contratista tiene ingresos o contratos asociados");
+//       }
+       asignacionesDAO.remove(objBorrar);    
     }
 
   
---
+++ src/java/Vista/asignacionesVista.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+import Logica.AsignacionesLogicaLocal;
+import Modelo.Asignaciones;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.component.commandbutton.CommandButton;
+import org.primefaces.component.inputtext.InputText;
+
+
+@Named(value = "asignacionesVista")
+@RequestScoped
+public class asignacionesVista {
+
+    @EJB
+    private AsignacionesLogicaLocal asignacionesLogica;
+
+    private List<Asignaciones> listaAsignaciones;
+    private InputText txthoras;
+    private InputText txtpresupuesto;
+    private InputText txtcantidad_proyectos;
+    private InputText txtId;
+    private CommandButton Registrar;
+    private CommandButton Modificar;
+    private Asignaciones selectedAsig;
+    public asignacionesVista() {
+    }
+
+    public AsignacionesLogicaLocal getAsignacionesLogica() {
+        return asignacionesLogica;
+    }
+
+    public void setAsignacionesLogica(AsignacionesLogicaLocal asignacionesLogica) {
+        this.asignacionesLogica = asignacionesLogica;
+    }
+
+    public List<Asignaciones> getListaAsignaciones() {
+        return listaAsignaciones;
+    }
+
+    public void setListaAsignaciones(List<Asignaciones> listaAsignaciones) {
+        this.listaAsignaciones = listaAsignaciones;
+    }
+
+    public InputText getTxthoras() {
+        return txthoras;
+    }
+
+    public void setTxthoras(InputText txthoras) {
+        this.txthoras = txthoras;
+    }
+
+    public InputText getTxtpresupuesto() {
+        return txtpresupuesto;
+    }
+
+    public void setTxtpresupuesto(InputText txtpresupuesto) {
+        this.txtpresupuesto = txtpresupuesto;
+    }
+
+    public InputText getTxtcantidad_proyectos() {
+        return txtcantidad_proyectos;
+    }
+
+    public void setTxtcantidad_proyectos(InputText txtcantidad_proyectos) {
+        this.txtcantidad_proyectos = txtcantidad_proyectos;
+    }
+
+    public InputText getTxtId() {
+        return txtId;
+    }
+
+    public void setTxtId(InputText txtId) {
+        this.txtId = txtId;
+    }
+
+    public CommandButton getRegistrar() {
+        return Registrar;
+    }
+
+    public void setRegistrar(CommandButton Registrar) {
+        this.Registrar = Registrar;
+    }
+
+    public CommandButton getModificar() {
+        return Modificar;
+    }
+
+    public void setModificar(CommandButton Modificar) {
+        this.Modificar = Modificar;
+    }
+
+    public Asignaciones getSelectedAsig() {
+        return selectedAsig;
+    }
+
+    public void setSelectedAsig(Asignaciones selectedAsig) {
+        this.selectedAsig = selectedAsig;
+    }
+    public void registrarAsignaciones() {
+      try {
+        Asignaciones nuevaAsig = new Asignaciones();
+        nuevaAsig.setHoras(Integer.parseInt(txthoras.getValue().toString()));
+        nuevaAsig.setCantidadProyectos(Integer.parseInt(txtcantidad_proyectos.getValue().toString()));
+        nuevaAsig.setPresupuestos(Float.parseFloat(txtpresupuesto.getValue().toString()));
+
+        asignacionesLogica.registrarAsignaciones(nuevaAsig);
+        
+        FacesContext.getCurrentInstance().addMessage(null,
+         new FacesMessage(FacesMessage.SEVERITY_INFO,"Mensaje",
+                 "La Asignacion  se ha Registrado Satisfactoriamente"));
+    } catch (Exception ex){
+      FacesContext.getCurrentInstance().addMessage(null,
+         new FacesMessage(FacesMessage.SEVERITY_ERROR,"Mensaje",
+                 ex.getMessage()));
+      Logger.getLogger(asignacionesVista.class.getName()).log(Level.SEVERE,null,ex);
+    }        
+       
+    }
+    
+     public void modificarDB() {
+      try {
+        Asignaciones nuevaAsig = new Asignaciones();
+        nuevaAsig.setHoras(Integer.parseInt(txthoras.getValue().toString()));
+        nuevaAsig.setCantidadProyectos(Integer.parseInt(txtcantidad_proyectos.getValue().toString()));
+        nuevaAsig.setPresupuestos(Float.parseFloat(txtpresupuesto.getValue().toString()));
+        
+        asignacionesLogica.modificarAsignaciones(nuevaAsig);
+        
+        FacesContext.getCurrentInstance().addMessage(null,
+         new FacesMessage(FacesMessage.SEVERITY_INFO,"Mensaje",
+                 "La asignaciones ha sido Modificada Satisfactoriamente"));
+    } catch (Exception ex){
+      FacesContext.getCurrentInstance().addMessage(null,
+         new FacesMessage(FacesMessage.SEVERITY_ERROR,"Mensaje",
+                 ex.getMessage()));
+      Logger.getLogger(asignacionesVista.class.getName()).log(Level.SEVERE,null,ex);
+    }        
+       
+    }
+     
+       public void eliminarEmpleado() {
+      try {
+       
+        asignacionesLogica.eliminarAsignaciones(selectedAsig);
+        
+        FacesContext.getCurrentInstance().addMessage(null,
+         new FacesMessage(FacesMessage.SEVERITY_INFO,"Mensaje",
+                 "Base de Datos Eliminada Satisfactoriamente"));
+    } catch (Exception ex){
+      FacesContext.getCurrentInstance().addMessage(null,
+         new FacesMessage(FacesMessage.SEVERITY_ERROR,"Mensaje",
+                 ex.getMessage()));
+      Logger.getLogger(asignacionesVista.class.getName()).log(Level.SEVERE,null,ex);
+    }        
+       
+    }
+}

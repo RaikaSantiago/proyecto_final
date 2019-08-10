90ba90d005a442c43a425793d294357018c2a8c4
RaikaSantiago <https://github.com/RaikaSantiago/Taller1JavaWeb.git>
2019-08-10 10:26:22 -0500

    Santiago - Pack Logicav2

---
+++ src/java/Logica/Requisito_ContenidoLogica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author ALEJANDRA
+ */
+@Stateless
+public class Requisito_ContenidoLogica implements Requisito_ContenidoLogicaLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
